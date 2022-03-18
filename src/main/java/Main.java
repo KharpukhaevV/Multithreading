67ac931055f1a6df1ee7b53d730ccb8a489e29ad
KharpukhaevV <vovan.20988@gmail.com>
2022-03-18 22:42:41 +0400

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        FixedThreadPool fixedThreadPool = new FixedThreadPool(10);
-//        fixedThreadPool.start();
-//        for (int i = 0; i < 5; i++) {
-//            fixedThreadPool.execute(() -> System.out.println("Print from : " + Thread.currentThread().getName()));
-//        }
-
-        ScalableThreadPool scalableThreadPool = new ScalableThreadPool(2, 10);
-        scalableThreadPool.start();
-        for (int i = 0; i < 100; i++) {
-            scalableThreadPool.execute(() -> System.out.println("Print from : " + Thread.currentThread().getName()));
+        FixedThreadPool fixedThreadPool = new FixedThreadPool(3);
+        fixedThreadPool.start();
+        for (int i = 0; i < 5; i++) {
+            fixedThreadPool.execute(() -> System.out.println("Print from : " + Thread.currentThread().getName()));
         }
+
+
+//        ScalableThreadPool scalableThreadPool = new ScalableThreadPool(2, 10);
+//        scalableThreadPool.start();
+//        for (int i = 0; i < 100; i++) {
+//            scalableThreadPool.execute(() -> System.out.println("Print from : " + Thread.currentThread().getName()));
+//        }
     }
 }
--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.BlockingQueue;
 
 public class Worker extends Thread{
-    private BlockingQueue<Runnable> workQueue;
+    private final BlockingQueue<Runnable> workQueue;
 
     public Worker(String name, BlockingQueue<Runnable> workQueue) {
         super(name);
@@ -10,6 +10,10 @@ public class Worker extends Thread{
 
     public void run() {
         while (workQueue.isEmpty()) {
+//            Runnable r;
+//            while ((r = workQueue.poll()) != null) {
+//                r.run();
+//            }
             try {
                 workQueue.take().run();
             } catch (InterruptedException e) {
